c31af8fde005e257293b9862ac9ac70bda35bf7a
Ata-92 <atakal92@gmail.com>
2021-07-10 19:22:27 +0300

    Add a model and migrate it

--- src/student/models.py
+++ src/student/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class StudentForm(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=100)
+    number = models.IntegerField(blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
--- src/student/views.py
+++ src/student/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import StudentForm
 
 # Create your views here.
@@ -6,8 +6,26 @@ def index(request):
     return render(request, "student/index.html")
 
 def student_page(request):
+
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            student_name = form.cleaned_data.get("first_name")
+            student_surname = form.cleaned_data.get("last_name")
+            student_number = form.cleaned_data.get("number")
+
+            print(student_name, student_surname, student_number)
+            print(form)
+            return redirect("index")
+
+        context = {
+            "form": form
+        }
+        return render(request, "student/student_form.html", context)
+
     form = StudentForm()
     context = {
         "form": form
     }
     return render(request, "student/student_form.html", context)
+
