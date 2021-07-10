24e7a99d22f849ecdeb0984bc542acb83ef1d661
Ata-92 <atakal92@gmail.com>
2021-07-10 19:31:33 +0300

    Add a ModelForm class

--- src/student/forms.py
+++ src/student/forms.py
@@ -1,7 +1,14 @@
 from django import forms
+from django.forms import fields
 from django.forms.fields import CharField
+from .models import Student
+# class StudentForm(forms.Form):
+#     first_name = forms.CharField(max_length=50, label="Your name")
+#     last_name = forms.CharField(max_length=100, label='Last Name')
+#     number = forms.IntegerField(required = False)
 
-class StudentForm(forms.Form):
-    first_name = forms.CharField(max_length=50, label="Your name")
-    last_name = forms.CharField(max_length=100, label='Last Name')
-    number = forms.IntegerField(required = False)
+class Student(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ["first_name", "last_name", "number"]
+        labels = {"first_name": "Adınız"}
