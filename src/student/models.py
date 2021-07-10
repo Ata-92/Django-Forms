7a02a727543eedbd890d96b2ba3fe0acdc2e9967
Ata-92 <atakal92@gmail.com>
2021-07-10 19:49:27 +0300

    Add a special method __str__

--- src/student/models.py
+++ src/student/models.py
@@ -6,3 +6,6 @@ class Student(models.Model):
     last_name = models.CharField(max_length=100)
     number = models.IntegerField(blank=True, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
